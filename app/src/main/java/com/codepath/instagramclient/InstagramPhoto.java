779d80aeb5494a1d313bd644413c3dcbedcbac43
Feng Guo <fengsterooni@gmail.com>
2015-04-28 18:05:32 -0700

    initial version

---
+++ app/src/main/java/com/codepath/instagramclient/InstagramPhoto.java
@@ -0,0 +1,9 @@
+package com.codepath.instagramclient;
+
+public class InstagramPhoto {
+    public String userName;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int likesCount;
+}
